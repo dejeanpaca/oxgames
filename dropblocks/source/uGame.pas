9001802ea859c272a96a85763d1d0eac1a3aa47f
Dejan Boras <de.jean.7777@protonmail.com>
2019-09-25 23:40:40 +0200

    Some work on setting up a game grid in DropBlocks

--- dropblocks/source/uGame.pas
+++ dropblocks/source/uGame.pas
@@ -4,19 +4,56 @@ UNIT uGame;
 INTERFACE
 
    USES
-      uStd, uLog, oxuTimer,
-      oxuEntity;
-
-CONST
-   GRID_WIDTH = 50;
-   GRID_HEIGHT = 50;
+      uStd, uLog,
+      {ox}
+      oxuTimer, oxuEntity,
+      {nase}
+      uBase;
 
 TYPE
+   TGridFlag = (
+      GRID_ELEMENT_SOLID,
+      GRID_ELEMENT_DIRTY
+   );
+
+   TGridFlags = set of TGridFlag;
+
+   PGridElement = ^TGridElement;
+
+   { TGridElement }
+
+   TGridElement = record
+      Flags: TGridFlags;
+      Shape: loopint;
+      Entity: oxTEntity;
+
+      function IsSolid(): boolean;
+      function IsDirty(): boolean;
+   end;
+
+
+   TGridArea = array[0..GRID_HEIGHT - 1, 0..GRID_WIDTH - 1] of TGridElement;
+
+   { TGrid }
+
+   TGrid = record
+      Area: TGridArea;
+      {is the grid dirty}
+      Dirty: boolean;
+
+      procedure SetPoint(x, y: loopint; what: TGridFlags);
+      procedure MarkDirty(x, y: loopint);
+      procedure MarkSolid(x, y: loopint);
+      function GetPoint(x, y: loopint): PGridElement;
+      function ValidPoint(x, y: loopint): boolean;
+   end;
 
    { TGame }
 
    TGame = record
       OnNew: TProcedures;
+
+      Grid: TGrid;
 
       procedure New();
    end;
@@ -25,6 +62,49 @@ VAR
    game: TGame;
 
 IMPLEMENTATION
+
+{ TGridElement }
+
+function TGridElement.IsSolid(): boolean;
+begin
+   Result := GRID_ELEMENT_SOLID in Flags;
+end;
+
+function TGridElement.IsDirty(): boolean;
+begin
+   Result := GRID_ELEMENT_DIRTY in Flags;
+end;
+
+{ TGrid }
+
+procedure TGrid.SetPoint(x, y: loopint; what: TGridFlags);
+begin
+   Area[y][x].Flags := Area[y][x].Flags + what;
+   Dirty := true;
+end;
+
+procedure TGrid.MarkDirty(x, y: loopint);
+begin
+   SetPoint(x, y, [GRID_ELEMENT_DIRTY]);
+end;
+
+procedure TGrid.MarkSolid(x, y: loopint);
+begin
+   SetPoint(x, y, [GRID_ELEMENT_SOLID]);
+end;
+
+function TGrid.GetPoint(x, y: loopint): PGridElement;
+begin
+   if(ValidPoint(x, y)) then
+      Result := @Area[y][x]
+   else
+      Result := nil;
+end;
+
+function TGrid.ValidPoint(x, y: loopint): boolean;
+begin
+   Result := (x >= 0) and (y >= 0) and (x < GRID_WIDTH) and (y < GRID_HEIGHT);
+end;
 
 { TGame }
 
--- dropblocks/source/uGrid.pas
+++ dropblocks/source/uGrid.pas
@@ -4,14 +4,14 @@ UNIT uGrid;
 INTERFACE
 
    USES
-      uStd, uColors,
+      uStd, uColors, StringUtils,
       {oX}
-      oxuScene, oxuEntity,
-      oxuComponent, oxuComponentDescriptors,
+      oxuMaterial, oxuScene, oxuEntity,
+      oxuComponent, oxuComponentDescriptors, oxuProjectionType,
       oxuCameraComponent,
-      oxuMaterial, oxumPrimitive, oxuPrimitiveModelComponent,
+      oxumPrimitive, oxuPrimitiveModelComponent, oxuPrimitiveModelEntities,
       {game}
-      uGame;
+      uBase, uGame;
 
 TYPE
    { TGridComponent }
@@ -39,19 +39,125 @@ VAR
 IMPLEMENTATION
 
 VAR
-   gridEntity: oxTEntity;
+   gridEntity,
+   gridBackground: oxTEntity;
+
+   Materials: record
+      GridBackground,
+      DefaultBlock: oxTMaterial;
+   end;
+
+function getElementMesh(x, y: loopint; out mesh: oxTPrimitiveModelComponent): PGridElement;
+begin
+   mesh := nil;
+   Result := game.Grid.GetPoint(x, y);
+
+   if(Result <> nil) and (Result^.Entity <> nil) then
+      mesh := oxTPrimitiveModelComponent(Result^.Entity.GetComponent('oxTPrimitiveModelComponent'));
+end;
+
+{ TGridComponent }
+
+function CreateMaterial(const name: string; color: TColor4ub): oxTMaterial;
+begin
+   Result := oxMaterial.Make();
+   Result.Name := name;
+   Result.SetColor('color', color);
+end;
 
 procedure TGridComponent.Load();
 begin
+   Materials.GridBackground := CreateMaterial('Background', cBlack4ub);
+   Materials.DefaultBlock := CreateMaterial('DefaultBlock', cWhite4ub);
 end;
 
 procedure TGridComponent.Update();
+var
+   x,
+   y: loopint;
+
+   mesh: oxTPrimitiveModelComponent;
+   element: PGridElement;
+
 begin
+   if(game.Grid.Dirty) then begin
+      for x := 0 to GRID_WIDTH - 1 do begin
+         for y := 0 to GRID_HEIGHT - 1 do begin
+            element := getElementMesh(x, y, mesh);
+
+            if element^.IsDirty() and (mesh <> nil) then
+               mesh.Model.SetMaterial(Materials.DefaultBlock);
+         end;
+      end;
+
+      game.Grid.Dirty := false;
+   end;
 end;
 
 function TGridComponent.GetDescriptor(): oxPComponentDescriptor;
 begin
    Result := @grid.Descriptor;
+end;
+
+procedure gridInitialize();
+var
+   i,
+   j,
+   gridSize: loopint;
+
+   element: PGridElement;
+
+   camera: oxTCameraComponent;
+   projection: oxPProjection;
+   w,
+   h,
+   halfGridW,
+   halfGridH: single;
+
+begin
+   if(gridEntity = nil) then
+      exit;
+
+   gridEntity.EmptyChildren();
+
+   camera := oxTCameraComponent(oxScene.GetComponentInChildren('oxTCameraComponent'));
+   projection := camera.GetProjection();
+
+   gridSize := GRID_WIDTH;
+   if(gridSize > GRID_HEIGHT) then
+      gridSize := GRID_HEIGHT;
+
+   if(projection^.p.GetWidth() >= projection^.p.GetHeight()) then begin
+      w := projection^.p.GetHeight() / gridSize / 2;
+      h := w;
+   end else begin
+      w := projection^.p.GetWidth() / gridSize / 2;
+      h := w;
+   end;
+
+   halfGridW := w * GRID_WIDTH - w;
+   halfGridH := h * GRID_HEIGHT - h;
+
+   for i := 0 to GRID_WIDTH - 1 do begin
+      for j := 0 to GRID_HEIGHT - 1 do begin
+         element := game.Grid.GetPoint(i, j);
+
+         element^.Entity := oxPrimitiveModelEntities.Plane();
+
+         element^.Entity.Name := sf(i) + 'x' + sf(j);
+         element^.Entity.SetPosition(i * (w * 2)  - halfGridW , j * (h * 2) - halfGridH, 0);
+         element^.Entity.SetScale(w, h, 1);
+
+         gridEntity.Add(element^.Entity);
+      end;
+   end;
+
+   gridBackground := oxPrimitiveModelEntities.Plane();
+
+   gridBackground.SetPosition(0, 0, -0.5);
+   gridBackground.SetScale(GRID_WIDTH * halfGridW, GRID_HEIGHT * halfGridH, -0.5);
+   oxTPrimitiveModelComponent(gridBackground.GetComponent('oxTPrimitiveModelComponent')).Model.SetMaterial(Materials.GridBackground);
+   gridBackground.Name := 'Background';;
 end;
 
 { TGridGlobal }
@@ -66,7 +172,10 @@ begin
 
    gridEntity.LoadComponentsInChildren();
 
+   gridInitialize();
+
    oxScene.Add(gridEntity);
+   oxScene.Add(gridBackground);
 end;
 
 procedure onNew();
