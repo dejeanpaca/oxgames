b82c11536a9c2f123b9047e965c15c266284c4c4
Dejan Boras <de.jean.7777@protonmail.com>
2020-02-12 21:35:41 +0100

    DropBlocks uses TGridElementsSize to determine grid size

--- dropblocks/source/uGrid.pas
+++ dropblocks/source/uGrid.pas
@@ -9,11 +9,11 @@ INTERFACE
       oxuPaths,
       oxuMaterial, oxuTexture, oxuTextureGenerate,
       oxuScene, oxuEntity,
-      oxuComponent, oxuComponentDescriptors, oxuProjectionType,
+      oxuComponent, oxuComponentDescriptors,
       oxuCameraComponent,
       oxumPrimitive, oxuPrimitiveModelComponent, oxuPrimitiveModelEntities,
       {game}
-      uBase, uGame, uDropBlocks, uBlocks;
+      uBase, uGame, uDropBlocks, uBlocks, uShared;
 
 TYPE
    {method which goes through current shape points in the grid}
@@ -121,16 +121,13 @@ end;
 procedure gridInitialize();
 var
    i,
-   j,
-   gridSize: loopint;
+   j: loopint;
+
+   gridSize: TGridElementsSize;
 
    element: PGridElement;
 
    camera: oxTCameraComponent;
-   projection: oxPProjection;
-   d,
-   halfGridW,
-   halfGridH: single;
 
 begin
    if(gridEntity = nil) then
@@ -139,20 +136,9 @@ begin
    gridEntity.EmptyChildren();
 
    camera := oxTCameraComponent(oxScene.GetComponentInChildren('oxTCameraComponent'));
-   projection := camera.GetProjection();
 
-   if(projection^.p.GetWidth() <= projection^.p.GetHeight()) then begin
-      gridSize := GRID_WIDTH;
-      d := projection^.p.GetWidth();
-   end else begin
-      gridSize := GRID_HEIGHT;
-      d := projection^.p.GetHeight();
-   end;
-
-   d := d / gridSize / 2;
-
-   halfGridW := d * GRID_WIDTH - d;
-   halfGridH := d * GRID_HEIGHT - d;
+   TGridElementsSize.Initialize(gridSize);
+   gridSize.Get(camera, GRID_WIDTH, GRID_HEIGHT);
 
    for i := 0 to GRID_WIDTH - 1 do begin
       for j := 0 to GRID_HEIGHT - 1 do begin
@@ -161,8 +147,8 @@ begin
          element^.Entity := oxPrimitiveModelEntities.Plane();
 
          element^.Entity.Name := sf(i) + 'x' + sf(j);
-         element^.Entity.SetPosition(i * d * 2  - halfGridW , j * d * 2 - halfGridH, 0);
-         element^.Entity.SetScale(d, d, 1);
+         element^.Entity.SetPosition(i * gridSize.d * 2  - gridSize.halfW , j * gridSize.d * 2 - gridSize.halfH, 0);
+         element^.Entity.SetScale(gridSize.d, gridSize.d, 1);
 
          gridEntity.Add(element^.Entity);
       end;
@@ -173,7 +159,7 @@ begin
    gridBackground := oxPrimitiveModelEntities.Plane();
 
    gridBackground.SetPosition(0, 0, -0.5);
-   gridBackground.SetScale(GRID_WIDTH * d, GRID_HEIGHT * d, 0);
+   gridBackground.SetScale(GRID_WIDTH * gridSize.d, GRID_HEIGHT * gridSize.d, 0);
 
    backgroundComponent := oxTPrimitiveModelComponent(gridBackground.GetComponent('oxTPrimitiveModelComponent'));
 
