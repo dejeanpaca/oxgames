60b3bba60f2897420e1be31282330a85395aa9dc
Dejan Boras <de.jean.7777@protonmail.com>
2020-02-17 23:21:01 +0100

    Improve shape preview appearance
    - Also remove uneeded AnimateLRMaterial() method

--- dropblocks/source/uBlocks.pas
+++ dropblocks/source/uBlocks.pas
@@ -16,14 +16,14 @@ TYPE
    { TBlocks }
 
    TBlocks = record
+      BlockTexture: oxTTexture;
+
       Materials: array[0..MAX_SHAPES - 1] of oxTMaterial;
       LRAnimationMaterial: oxTMaterial;
       {material for inserted lines}
       Rock,
       {default block material}
       DefaultMaterial: oxTMaterial;
-
-      procedure AnimateLRMaterial(elapsed: single);
    end;
 
 VAR
@@ -34,29 +34,21 @@ IMPLEMENTATION
 procedure init();
 var
    i: loopint;
-   tex: oxTTexture;
 
 begin
-   oxTextureGenerate.Generate(oxPaths.Find('textures' + DirectorySeparator + 'block.png'), tex);
+   oxTextureGenerate.Generate(oxPaths.Find('textures' + DirectorySeparator + 'block.png'), blocks.BlockTexture);
 
-   if(tex = nil) then
+   if(blocks.BlockTexture = nil) then
       log.w('Failed loading block texture');
 
    for i := 0 to MAX_SHAPES - 1 do begin
-     blocks.Materials[i] := CreateMaterial('shape_' + sf(i), Shapes.Colors[i], tex);
+     blocks.Materials[i] := CreateMaterial('shape_' + sf(i), Shapes.Colors[i], blocks.BlockTexture);
    end;
 
-   blocks.Rock := CreateMaterial('default',  TColor4ub.Create(127, 127, 127, 255), tex);
-   blocks.DefaultMaterial := CreateMaterial('default', cWhite4ub, tex);
+   blocks.Rock := CreateMaterial('default',  TColor4ub.Create(127, 127, 127, 255), blocks.BlockTexture);
+   blocks.DefaultMaterial := CreateMaterial('default', cWhite4ub, blocks.BlockTexture);
 
-   blocks.LRAnimationMaterial := CreateMaterial('lr_animation', cWhite4ub, tex);
-end;
-
-{ TBlocks }
-
-procedure TBlocks.AnimateLRMaterial(elapsed: single);
-begin
-   LRAnimationMaterial.SetColor('color', cWhite4ub.Lighten(elapsed));
+   blocks.LRAnimationMaterial := CreateMaterial('lr_animation', cWhite4ub, blocks.BlockTexture);
 end;
 
 INITIALIZATION
