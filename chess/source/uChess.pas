cb133945d2b539d3dc21d3c72b3c0e1788b46072
Dejan Boras <de.jean.7777@protonmail.com>
2020-03-03 23:37:43 +0100

    Start implementing some chess logic

---
+++ chess/source/uChess.pas
@@ -0,0 +1,76 @@
+{$INCLUDE oxdefines.inc}
+UNIT uChess;
+
+INTERFACE
+
+   USES
+      uStd;
+
+TYPE
+   TPieceType = (
+      PIECE_NONE
+      PIECE_PAWN,
+      PIECE_KNIGHT,
+      PIECE_BISHOP,
+      PIECE_ROOK,
+      PIECE_QUEEN,
+      PIECE_KING
+   );
+
+   TPlayer = (
+      PLAYER_WHITE,
+      PLAYER_BLACK
+   );
+
+   TMoveAction = (
+      ACTION_MOVE,
+      ACTION_EAT
+   );
+
+   TPiece = record
+      {type of piece}
+      Piece: TPieceType;
+      {what player the piece belongs to}
+      Player: TPlayer;
+   end;
+
+   TBoard = array[0..7, 0..7] of TPiece;
+
+   TPiecePosition = record
+      x,
+      y: loopint;
+   end;
+
+   TMove = record
+      From: TPiecePosition;
+      To: TPiecePosition;
+      Piece: TPieceType;
+      Action: TMoveAction;
+   end;
+
+   TMovesList = specialize TSimpleList<TMove>;
+
+   { TChess }
+
+   TChess = record
+      Board: TBoard;
+
+      {get allowed moves for the piece on the given coordinates}
+      function GetMoves(x, y: loopint): TMovesList;
+      {get all allowed moves for the entire board for a player}
+      function GetAllMoves(player: TPlayer): TMovesList;
+   end;
+
+VAR
+   chess: TChess;
+
+IMPLEMENTATION
+
+{ TChess }
+
+function GetMoves(x, y: loopint): TMovesList;
+begin
+   TMovesList.Initialize(Result);
+end;
+
+END.
