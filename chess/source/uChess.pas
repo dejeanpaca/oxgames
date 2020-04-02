2e376596f6b828fd0fae177e165ddf1e07602a68
Dejan Boras <de.jean.7777@protonmail.com>
2020-04-02 23:42:49 +0200

    Improve chess game basics
    - TPiece has some basic methods
    - Starting player can be set
    - We can now setup the board
    - Start piece names with uppercase

--- chess/source/uChess.pas
+++ chess/source/uChess.pas
@@ -27,11 +27,16 @@ TYPE
       ACTION_EAT
    );
 
+   { TPiece }
+
    TPiece = record
       {type of piece}
       Piece: TPieceType;
       {what player the piece belongs to}
       Player: TPlayer;
+
+      procedure Place(usePiece: TPieceType; usePlayer: TPlayer);
+      procedure Clear();
    end;
 
    TBoard = array[0..7, 0..7] of TPiece;
@@ -54,30 +59,58 @@ TYPE
 
    TChess = record
       Board: TBoard;
+      StartingPlayer: TPlayer;
 
       {get allowed moves for the piece on the given coordinates}
       function GetMoves(x, y: loopint): TMovesList;
       {get all allowed moves for the entire board for a player}
       function GetAllMoves(player: TPlayer): TMovesList;
+
+      procedure ResetBoard();
    end;
 
 CONST
    PIECE_TYPE_MAX = loopint(PIECE_KING);
 
    PIECE_NAMES: array[0..PIECE_TYPE_MAX] of StdString = (
-      'none',
-      'pawn',
-      'knight',
-      'bishop',
-      'rook',
-      'queen',
-      'king'
+      'None',
+      'Pawn',
+      'Knight',
+      'Bishop',
+      'Rook',
+      'Queen',
+      'King'
+   );
+
+   PIECE_PLACEMENT: array[0..7] of TPieceType = (
+      PIECE_ROOK,
+      PIECE_KNIGHT,
+      PIECE_BISHOP,
+      PIECE_QUEEN,
+      PIECe_KING,
+      PIECE_BISHOP,
+      PIECE_KNIGHT,
+      PIECE_ROOK
    );
 
 VAR
    chess: TChess;
 
 IMPLEMENTATION
+
+{ TPiece }
+
+procedure TPiece.Place(usePiece: TPieceType; usePlayer: TPlayer);
+begin
+   Piece := usePiece;
+   Player := usePlayer;
+end;
+
+procedure TPiece.Clear();
+begin
+   Piece := PIECE_NONE;
+   Player := PLAYER_BLACK;
+end;
 
 { TChess }
 
@@ -90,5 +123,44 @@ function TChess.GetAllMoves(player: TPlayer): TMovesList;
 begin
    TMovesList.Initialize(Result);
 end;
+
+procedure TChess.ResetBoard();
+var
+   i,
+   j: loopint;
+   playerFirst,
+   playerSecond: TPlayer;
+
+begin
+   for i := 0 to 7 do begin
+      for j := 0 to 7 do begin
+         Board[i][j].Clear();
+      end;
+   end;
+
+   if(StartingPlayer = PLAYER_BLACK) then begin
+      playerFirst := PLAYER_BLACK;
+      playerSecond := PLAYER_WHITE;
+   end else begin
+      playerFirst := PLAYER_WHITE;
+      playerSecond := PLAYER_BLACK;
+   end;
+
+   {place pawns}
+   for j := 0 to 7 do begin
+      Board[6][j].Place(PIECE_PAWN, playerSecond);
+      Board[1][j].Place(PIECE_PAWN, playerFirst);
+   end;
+
+   {place other pieces}
+   for j := 0 to 7 do begin
+      Board[7][7 - j].Place(PIECE_PLACEMENT[7 - j], playerSecond);
+      Board[0][j].Place(PIECE_PLACEMENT[j], playerFirst);
+   end;
+end;
+
+INITIALIZATION
+   chess.StartingPlayer := PLAYER_WHITE;
+   chess.ResetBoard();
 
 END.
