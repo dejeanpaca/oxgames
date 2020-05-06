fecf1c8e63519f133d0fdad0566d266fb92bf441
Dejan Boras <de.jean.7777@protonmail.com>
2020-05-06 19:11:40 +0200

    Chess game tiles has some selection mechanism

--- chess/source/uGame.pas
+++ chess/source/uGame.pas
@@ -15,11 +15,16 @@ TYPE
    { TGameGlobal }
 
    TGameGlobal = record
-      OnNew: TProcedures;
+      OnNew,
+      OnSelectedTile,
+      OnUnselectedTile: TProcedures;
 
       SelectedTile: oxTPoint;
 
       procedure New();
+
+      {select a tile to play}
+      procedure SelectTile(x, y: loopint);
    end;
 
 VAR
@@ -33,9 +38,34 @@ procedure TGameGlobal.New();
 begin
    chess.CurrentPlayer := chess.StartingPlayer;
    chess.ResetBoard();
+
+   {unselect tile}
+   SelectedTile.x := -1;
+   SelectedTile.y := -1;
+
+   OnSelectedTile.Call();
+end;
+
+procedure TGameGlobal.SelectTile(x, y: loopint);
+begin
+   {if given tile was already selected, unselect it}
+   if(SelectedTile.x = x) and (SelectedTile.y = y) then begin
+      OnUnselectedTile.Call();
+
+      SelectedTile.x := -1;
+      SelectedTile.y := -1;
+   end else begin
+      {unselect given tile}
+      SelectedTile.x := x;
+      SelectedTile.y := y;
+
+      OnSelectedTile.Call();
+   end;
 end;
 
 INITIALIZATION
    TProcedures.Initialize(game.OnNew);
+   TProcedures.Initialize(game.OnSelectedTile);
+   TProcedures.Initialize(game.OnUnselectedTile);
 
 END.
